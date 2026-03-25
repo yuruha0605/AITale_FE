f0453fddc5df794d793b26ed992452774c9b1567
박명환 <jackpark5872@yonsei.ac.kr>
2026-03-25 12:35:54 +0900

    first

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import TestPage from "./pages/test/TestPage";
 import TestResultPage from "./pages/test/TestResultPage";
 
 import MainPage from "./pages/main/MainPage";
-import MyPage from "./pages/mypage/MyPage";
 
 import StoryReadPage from "./pages/story/StoryReadPage";
 
@@ -21,7 +20,6 @@ import QuizIntroPage from "./pages/quiz/QuizIntroPage";
 import QuizPage from "./pages/quiz/QuizPage";
 import QuizResult from "./pages/quiz/QuizResult";
 
-import ReportPage from "./pages/report/ReportPage";
 import MyReport from "./pages/myreport/MyReport";
 
 
@@ -42,10 +40,8 @@ export default function App() {
 
           <Route path="/story/:id" element={<StoryReadPage />} />
 
-          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/mypage" element={<Navigate to="/myreport" replace />} />
 
-          <Route path="*" element={<Navigate to="/" replace />} />
-  
           <Route path="/main" element={<MainPage />} />
 
 
@@ -55,7 +51,7 @@ export default function App() {
           <Route path="/quiz/extra/:id" element={<QuizExtraPage />} />
           <Route path="/quiz/extra/result" element={<QuizFinalResult />} />
 
-          <Route path="/report" element={<ReportPage />} />
+          <Route path="/report" element={<Navigate to="/myreport" replace />} />
 
           <Route path="/myreport" element={<MyReport />} />
 
